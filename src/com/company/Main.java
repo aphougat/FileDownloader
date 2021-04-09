c8f048309968089fa8ab6e28e32078c699cce933
abhayphougat <abhayphougat@gmail.com>
2021-04-09 15:36:16 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(300);
+        for(int i=100000; i <= 150000; i++)
+        {
+            Task task = new Task(i);
+            executor.execute(task::run);
+        }
+    }
+}
---
+++ src/com/company/Task.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+
+public class Task implements Runnable{
+
+    private int number;
+
+    public Task(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public void run() {
+        String url = "YOURURL";
+
+        try {
+            URL newURL = new URL(url);
+            InputStream in = newURL.openStream();
+            ReadableByteChannel rbc = Channels.newChannel(in);
+            FileOutputStream fos = new FileOutputStream("/YOURPATH"+number+".pdf");
+            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+        }
+        catch (IOException ex)
+        {
+            System.out.println("file not found "+url);
+        }
+    }
+}
