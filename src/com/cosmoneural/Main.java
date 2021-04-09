16407b847c9d4667610b34226c39ae1ee096a5b8
abhayphougat <abhayphougat@gmail.com>
2021-04-09 15:42:56 +0530

    initial commit

--- src/com/cosmoneural/Main.java
+++ src/com/cosmoneural/Main.java
@@ -6,9 +6,13 @@ import java.util.concurrent.ThreadPoolExecutor;
 public class Main {
 
 
+    private static final int THREAD_COUNT = 300;
+    private static final int MIN_COUNT = 100000;
+    private static final int MAX_COUNT = 150000;
+
     public static void main(String[] args) {
-        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(300);
-        for(int i=100000; i <= 150000; i++)
+        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(THREAD_COUNT);
+        for(int i=MIN_COUNT; i <= MAX_COUNT; i++)
         {
             Task task = new Task(i);
             executor.execute(task);
